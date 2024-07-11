3471c0271cc878ebc6dae9d08fccb1c18cdaba33
cvergarabah <c_vergara@hotmail.com>
2024-07-11 10:24:52 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,138 @@
+from utils import *
+import pandas as pd
+from modal import Image, Secret, Mount, Volume, App, asgi_app
+from fastapi import FastAPI, Response, Query, File, UploadFile, Request
+import time
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Annotated
+import os
+import json
+import re
+import io
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+plt.style.use('seaborn-white')
+
+
+image = (Image.micromamba()
+         .pip_install("numpy==1.23.5", "pandas==1.5.3", "openpyxl==3.1.2",
+                      "matplotlib==3.7.1", "seaborn==0.12.2", "python-dotenv==1.0.0", "PyQt5==5.15.10",
+                      "google-cloud-aiplatform==1.48.0",
+                      )
+         )
+
+app = App(name="fastapi-genai-demo-v1", image=image)
+
+
+class MyMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers['X-Process-Time'] = str(process_time)
+        return response
+
+
+genai_app = FastAPI(title='DataAnalystGenAIAPI',
+                        summary="Data Analyst GenAi API", version="1.0",
+                        contact={"name": "Cristian Vergara",
+                                 "email": "cvergara@geti.cl"})
+
+genai_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=False,
+    allow_methods=['*'],
+    allow_headers=['*'])
+genai_app.add_middleware(MyMiddleware)
+
+
+@genai_app.get("/")
+def read_root():
+    return {"Root": "Root_test"}
+
+
+@genai_app.get("/gemini-analyst")
+async def generate_output(user_query: str):
+    try:
+        # for now just loading a test xlsx
+        file = 'df_test.xlsx'
+        df = load_dataframe(f'{file}')
+        print(df.head())
+        response = analyze_table_gemini(query=user_query, df=df)
+        print('Python Snippet Generated: \n')
+        print(response)
+        if 'python' in response:
+            local_vars, output = execute_code(response, df=df)
+
+        if 'plt.' in response:
+
+            try:
+                fig = local_vars['plt'].gcf()
+                # Save it to a BytesIO object
+                # buf = io.BytesIO()
+                # fig.savefig(buf, format='png')
+                # buf.seek(0)
+                output = io.BytesIO()
+                FigureCanvas(fig).print_png(output)
+                return Response(output.getvalue(), media_type="image/png")
+
+            except Exception as e:
+                try:
+                    df_temp = local_vars['df_temp']
+                    return df_temp.to_json(orient='records')
+
+                except Exception as e:
+                    output = {
+                        "error": str(e),
+                        "output": "intente de nuevo probando con otra query"
+                    }
+                    return output
+
+        else:
+            try:
+                df_temp = local_vars['df_temp']
+                # Convert the DataFrame to JSON
+                return df_temp.to_json(orient='records')
+
+            except Exception as e:
+
+                try:
+                    output = {
+                        "response": str(response),
+                    }
+                    return output
+
+                except Exception as e:
+
+                    output = {
+                        "error": str(e),
+                        "output": "intente de nuevo probando con otra query"
+                    }
+                    return output
+
+    except Exception as e:
+        output = {
+            "error": str(e),
+            "output": "intente de nuevo probando con otra query"
+        }
+        return output
+
+
+@app.function(image=image,
+              secret=Secret.from_name("automatch-secret-keys"),
+              mounts=[Mount.from_local_file("key.json",
+                      remote_path="/root/key.json"),
+                      Mount.from_local_file("df_test.xlsx",
+                                            remote_path="/root/df_test.xlsx")
+                      ],
+              _allow_background_volume_commits=True,
+              timeout=999)  # schedule=Period(minutes=30)
+@asgi_app(label='fastapi-genai-demo-v1')
+def genai_fastapi_app():
+    # check available GPUs
+    print('####### GenAI v1.0 ####### \n'
+          '#### test ####')
+
+    return genai_app
---
+++ utils.py
@@ -0,0 +1,128 @@
+import os
+import pandas as pd
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
+import matplotlib.pyplot as plt
+import io
+import json
+import contextlib
+from dotenv import load_dotenv
+# env
+load_dotenv()
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
+PROJECT_ID = os.environ["PROJECT_ID"]
+LOCATION = os.environ["LOCATION"]
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+
+def parse_null_list(value):
+    if pd.isnull(value):
+        parse = '[]'
+        return parse
+    else:
+        return value
+
+def load_dataframe(file_path: str):
+    try:
+        if file_path.endswith('.csv'):
+            try:
+                df = pd.read_csv(file_path, index_col=0)
+            except:
+                df = pd.read_csv(file_path, index_col=0, delimiter=';')
+
+        elif file_path.endswith(('.xls', '.xlsx')):
+            # especial pa prueba con json col:
+            if 'df_test' in file_path:
+                df = pd.read_excel(file_path, engine='openpyxl')
+
+                df['Información extendida'] = df['Información extendida'].apply(json.loads)
+                json_df = pd.json_normalize(df['Información extendida'])
+                df = df.drop(columns=['Información extendida'])
+                df = pd.concat([df, json_df], axis=1)
+
+            else:
+                df = pd.read_excel(file_path, engine='openpyxl')
+
+        df = df.convert_dtypes()
+        df.columns = (df.columns.str.replace(' ', '_').str.lower().
+                      str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+
+        return df
+
+    except Exception as e:
+        output = {
+            "error": str(e),
+        }
+        return output
+
+
+def execute_code(snippet, df: pd.DataFrame):
+    # Strip the code snippet
+    code = snippet.strip().strip('```python').strip('```').strip()
+    local_vars = {'df': df, 'plt': plt}
+    # Redirect standard output to capture `print()` statements
+    output_capture = io.StringIO()
+    try:
+        with contextlib.redirect_stdout(output_capture):
+            exec(code, globals(), local_vars)
+        output = output_capture.getvalue()
+        return local_vars, output
+
+    except Exception as e:
+        output = {
+            "error": str(e),
+        }
+        return output
+
+
+def analyze_table_gemini(query: str, df: pd.DataFrame):
+    generation_config = {
+        "max_output_tokens": 8192,  # max
+        "temperature": 1,
+        "top_p": 0.95,
+    }
+
+    safety_settings = {
+        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }
+    if len(df) > 50:
+        df_sample = df.sample(n=50, random_state=42)
+    else:
+        df_sample = df
+
+    df_parsed = df_sample.to_string(index=False) # random sample. Little hack
+    model = GenerativeModel(
+        model_name="gemini-1.5-pro-001",
+        system_instruction=[
+            "Eres un asistente de IA experto en análisis de datos y programación en el lenguaje python.",
+            f"Debes responder preguntas relacionadas a la siguiente tabla de datos: {df_parsed}.",
+            "El usuario te proporcionará una consulta en lenguaje natural y debes responderla entregando un código en python.",
+            "Usa el nombre 'df' para la tabla que contenga la data. No vuelvas a generar la data en el código.",
+            f"Si vas a filtrar columnas de la tabla, siempre usa las siguientes columnas: {df.columns}."
+            "Piensa paso a paso, verificando que los formatos y tipos de datos sean los correctos y siempre importa las librerias necesarias en el codigo.",
+            "No imprimas comentarios en el código. (no uses #) y utiliza el comando print para imprimir la tabla con los datos de la consulta.",
+            "En el caso que tengas que imprimir una tabla con la respuesta final, llama a esta tabla: df_temp en el código generado."
+            "En el caso de que tengas que graficar, ocupa un fig_size fijo de (5,5) y siempre usa tight_layout.",
+            "Si no respondiste generando código en python, siempre respondes en español",
+            ])
+
+    prompt = f"""
+             User input: {query}
+             Answer:
+            """
+
+    contents = [prompt]
+
+    responses = model.generate_content(contents,
+                                       generation_config=generation_config,
+                                       safety_settings=safety_settings,
+                                       stream=False)  # Si True -> la parte de text tiene que ser iterada
+
+    # for response in responses:
+    #    print(response.text, end="")
+    return responses.text
